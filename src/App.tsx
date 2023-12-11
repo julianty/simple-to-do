805a5a06ca7fc9bfec36eea1ea8fcb2e18dd3301
julianty <alexanderjulianty@gmail.com>
2023-12-11 11:50:37 -0800

    Sets initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import { MantineProvider } from "@mantine/core";
+import {
+  MantineProvider,
+  Stack,
+  Group,
+  ActionIconGroup,
+  ActionIcon,
+} from "@mantine/core";
 
-import { Box } from "@mantine/core";
 import "@mantine/core/styles.css";
+import { AppShell, Center, Title } from "@mantine/core";
+import ToDoCard from "./components/ToDoCard";
+
 function App() {
   return (
     <MantineProvider>
-      <Box bg="red.5">A box</Box>
+      <AppShell header={{ height: 60 }} padding="md">
+        <AppShell.Header withBorder={false}>
+          <Center>
+            <Title>My As Yet Unnamed To Do App</Title>
+          </Center>
+        </AppShell.Header>
+        <AppShell.Main>
+          <Stack align="center">
+            <Group>
+              <ActionIcon />
+              <ActionIcon />
+              <ActionIcon />
+            </Group>
+            <Group>
+              <ToDoCard></ToDoCard>
+              <ToDoCard></ToDoCard>
+            </Group>
+          </Stack>
+        </AppShell.Main>
+      </AppShell>
     </MantineProvider>
   );
 }
