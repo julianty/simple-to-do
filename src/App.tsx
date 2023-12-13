8555310b1da52120f73e5c15aa91e9ec9ef660c2
julianty <alexanderjulianty@gmail.com>
2023-12-13 11:28:55 -0800

    Adds firebase config file to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import {
-  MantineProvider,
-  Stack,
-  Group,
-  ActionIconGroup,
-  ActionIcon,
-} from "@mantine/core";
+import { initializeApp } from "firebase/app";
 
 import "@mantine/core/styles.css";
+import { MantineProvider, Stack, Group, ActionIcon } from "@mantine/core";
 import { AppShell, Center, Title } from "@mantine/core";
 import ToDoCard from "./components/ToDoCard";
+import { IconPlus } from "@tabler/icons-react";
+
+// TODO: Put into .env file
+const firebaseConfig = {
+  apiKey: "AIzaSyBSbhodvsr3A1ftaY7ZzPoY8ynV2iGDg64",
+  authDomain: "simple-to-do-1765a.firebaseapp.com",
+  projectId: "simple-to-do-1765a",
+  storageBucket: "simple-to-do-1765a.appspot.com",
+  messagingSenderId: "1080345089585",
+  appId: "1:1080345089585:web:0a199d0e17740b7446e710",
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
@@ -21,10 +29,11 @@ function App() {
         </AppShell.Header>
         <AppShell.Main>
           <Stack align="center">
+            {/* Might be best to have this Group be a separate component */}
             <Group>
-              <ActionIcon />
-              <ActionIcon />
-              <ActionIcon />
+              <ActionIcon>
+                <IconPlus></IconPlus>
+              </ActionIcon>
             </Group>
             <Group>
               <ToDoCard></ToDoCard>
