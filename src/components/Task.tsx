47dd89802681a882f3131ae76f3f9e35fdcfb0b1
julianty <alexanderjulianty@gmail.com>
2023-12-18 13:29:22 -0800

    Adds a hover effect to tasks

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,5 +1,14 @@
-import { Text, Checkbox } from "@mantine/core";
-import React, { useState } from "react";
+import { Group, Checkbox, TextInput } from "@mantine/core";
+import React, { MouseEventHandler, useState } from "react";
+import { useHover } from "@mantine/hooks";
+import { IconX } from "@tabler/icons-react";
+
+interface CloseButtonProps {
+  clickHandler: MouseEventHandler;
+}
+function CloseButton({ clickHandler }: CloseButtonProps): React.ReactNode {
+  return <IconX onClick={clickHandler} />;
+}
 
 interface TaskProps {
   text: string;
@@ -7,16 +16,28 @@ interface TaskProps {
 export function Task(
   props: React.PropsWithChildren<TaskProps>
 ): React.ReactNode {
+  const { hovered, ref } = useHover();
   const text = props.text;
   const [checked, setChecked] = useState(false);
   function clickHandler(e: React.MouseEvent) {
     e.preventDefault;
     checked === false ? setChecked(true) : setChecked(false);
   }
+  function deleteItem(e: React.MouseEvent) {
+    e.preventDefault;
+    console.log("delete this task");
+  }
   return (
-    <Checkbox
-      onClick={clickHandler}
-      label={<Text td={checked ? "line-through" : ""}>{text}</Text>}
-    />
+    <Group ref={ref}>
+      <Checkbox onClick={clickHandler} />
+      <TextInput
+        disabled={checked ? true : false}
+        variant="unstyled"
+        defaultValue={text}
+        rightSection={
+          hovered ? <CloseButton clickHandler={deleteItem} /> : null
+        }
+      ></TextInput>
+    </Group>
   );
 }
